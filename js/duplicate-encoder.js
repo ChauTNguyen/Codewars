b015b920658c4d86b2db451d6fe6d291d4c5b641
Chau Nguyen <chautnguyen96@gmail.com>
2017-02-04 23:42:34 -0800

    Added `duplicate-encoder.js`

---
+++ js/duplicate-encoder.js
@@ -0,0 +1,31 @@
+function duplicateEncode(word) {
+    var res = [];
+    var arr = word.toLowerCase().split('');
+
+    for (var i = 0; i < arr.length; ++i) {
+        var c = arr[i];
+
+        if (arr.indexOf(c) === arr.lastIndexOf(c)) {
+            res.push('(');
+        } else {
+            res.push(')');
+        }
+    }
+
+    return res.join('');
+}
+
+function duplicateEncode(word) {
+    /* Using more array methods! */
+    return word
+            .toLowerCase()
+            .split('')
+            .map(function(c, i, arr) {
+                return arr.indexOf(c) === arr.lastIndexOf(c) ? '(' : ')'
+            })
+            .join('');
+}
+
+/* Pure ES6 solution. */
+const duplicateEncode = (word) =>
+              word.toLowerCase().split('').map((c, i, arr) => arr.indexOf(c) === arr.lastIndexOf(c) ? '(' : ')').join('');
